2a065f30797a484a517e32d1416dd0ad0e788abc
Yasmin Hillary Carvalho dos reis <yasmincarvalhoreis23@gmail.com>
2025-03-30 17:01:19 -0300

    Delete curso directory

--- curso/index.php
+++
@@ -1,7 +0,0 @@
-<?php
- include_once("templates/header.php");
-?>
-<h1> Olá mundo!</h1>
-<?php
- include_once("templates/footer.php");
-?>
--- curso/templates/footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
- <p>Hora de Codar &copy; 2025 </p>
-</footer>
-</body>
-</html>
